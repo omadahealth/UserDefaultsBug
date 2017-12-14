082f64c56156ae1436833dd7f143679919a20b78
Paul Zabelin <paul@omadahealth.com>
2017-12-14 10:44:49 -0800

    Initial Commit

---
+++ DefaultsBug/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DefaultsBug
+//
+//  Created by Omada Developer on 12/14/17.
+//  Copyright © 2017 Omada Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
